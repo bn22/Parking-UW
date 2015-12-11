5d003c2d68738872c0fb0f5b267afc4e7f06f2bb
ahnj12 <ahnj12@uw.edu>
2015-12-10 18:37:03 -0800

    set up storyboard

---
+++ ParseStarterProject/ParkingCell.swift
@@ -0,0 +1,13 @@
+//
+//  ParkingCell.swift
+//  ParkingUW
+//
+//  Created by iGuest on 12/10/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class ParkingCell {
+    
+}
